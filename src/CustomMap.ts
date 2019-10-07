36c7eba1dadd80aba79d4149ed31a9723f2f875c
burnsco <coreymburns@gmail.com>
2019-10-07 14:41:29 -0400

    basic maps class in ts

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,10 +1,30 @@
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
-  constructor() {
-    this.googleMap = new google.maps.Map(document.getElementById('map'), {
+  constructor(elementId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(elementId), {
       center: { lat: 0, lng: 0 },
       zoom: 1
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
     });
   }
 }
